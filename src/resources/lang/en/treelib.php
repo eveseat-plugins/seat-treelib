a66b14277527507f91c9aa6b34063a7150e99990
recursive_tree <recursivetreemail@gmail.com>
2022-11-14 10:38:04 +0100

    clarify settings name

--- src/resources/lang/en/treelib.php
+++ src/resources/lang/en/treelib.php
@@ -2,7 +2,7 @@
 
 return [
     "user_sidebar_menu"=>"User",
-    "treelib_settings"=>"Settings",
+    "treelib_settings"=>"Plugin Settings",
     "settings_giveaways_disabled_text"=>"EVE Partner Giveaways are disabled. You can opt in with the button below.",
     "settings_giveaways_enabled_text"=>"EVE Partner Giveaways are enabled. You can opt out of giveaways with the button below.",
     "settings_giveaway_settings"=>"Giveaway Settings"
