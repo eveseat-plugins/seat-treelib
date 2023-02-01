f8e6d4d8a29d929fdf248f323672529100e530fb
recursive_tree <recursivetreemail@gmail.com>
2023-02-01 15:25:27 +0100

    add type hint

--- src/Prices/AbstractPriceProvider.php
+++ src/Prices/AbstractPriceProvider.php
@@ -3,12 +3,14 @@
 namespace RecursiveTree\Seat\TreeLib\Prices;
 
 use RecursiveTree\Seat\TreeLib\Helpers\ItemList;
+use RecursiveTree\Seat\TreeLib\Helpers\SimpleItemWithPrice;
 
 abstract class AbstractPriceProvider
 {
     /**
     * @param ItemList $items
     * @param PriceProviderSettings $settings
+     * @return SimpleItemWithPrice[]
     */
     public static abstract function getPrices($items,$settings);
 }
