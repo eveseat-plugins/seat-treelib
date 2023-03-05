44b12a3abccf17eae764f1cf99c2ac3e6214ff1d
recursive_tree <recursivetreemail@gmail.com>
2023-03-05 11:31:21 +0100

    fix priceprovider settings

--- src/Config/treelib.priceproviders.php
+++ src/Config/treelib.priceproviders.php
@@ -1,11 +1,11 @@
 <?php
 return [
-    '\RecursiveTree\Seat\TreeLib\Prices\EvePraisalPriceProvider' => [
-        'class'=>'\RecursiveTree\Seat\TreeLib\Prices\EvePraisalPriceProvider',
+    \RecursiveTree\Seat\TreeLib\Prices\EvePraisalPriceProvider::class => [
+        'class'=>\RecursiveTree\Seat\TreeLib\Prices\EvePraisalPriceProvider::class,
         'name'=>'EvePraisal Prices'
     ],
-    '\RecursiveTree\Seat\TreeLib\Prices\CCPPricesPriceProvider' => [
-        'class'=>'\RecursiveTree\Seat\TreeLib\Prices\CCPPricesPriceProvider',
+    \RecursiveTree\Seat\TreeLib\Prices\CCPPricesPriceProvider::class => [
+        'class'=>\RecursiveTree\Seat\TreeLib\Prices\CCPPricesPriceProvider::class,
         'name'=>'CCP Prices'
     ],
 ];
